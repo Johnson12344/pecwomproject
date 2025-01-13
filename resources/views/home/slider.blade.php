cdae946975e27bd515633767d5df939d3d6e9376
Femi Ogunbanwo <ogunbanwofemi2000@gmail.com>
2025-01-13 12:17:13 +0100

    stupid commit

--- resources/views/home/slider.blade.php
+++ resources/views/home/slider.blade.php
@@ -6,14 +6,13 @@
             <div class="container-fluid">
               <div class="row">
                 <div class="col-md-7">
-                  {{-- <div class="detail-box">
+                  <div class="detail-box">
                     <h1>
                       Welcome To <br>
-                      PECWOM
+                      housewife
                     </h1>
                     <p>
-                        Welcome to PECWOM, your one-stop online shop for high-quality products at unbeatable prices. Our mission is simple: to provide fast, secure, and enjoyable shopping with a wide range of items across electronics, fashion, home goods, and more.
-                        At PECWOM, we focus on delivering exceptional value with affordable prices, fast shipping, and excellent customer service. We curate the best products from trusted brands to ensure quality, reliability, and satisfaction. Your shopping experience is our top priority, and we’re committed to offering a seamless, secure, and convenient process every step of the way. <br>
+                        W//////// to ensure quality, reliability, and satisfaction. Your shopping experience is our top priority, and we’re committed to offering a seamless, secure, and convenient process every step of the way. <br>
                         Thank you for choosing PECWOM—where quality meets convenience.                    </p>
                     <a href="https://www.instagram.com/pecwommakeover/">
                       Contact Us
@@ -33,4 +32,4 @@
 
       </div>
     </div>
-  </section> --}}
+  </section>
